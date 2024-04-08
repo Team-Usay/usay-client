1cf2061af5230a897cb97a2e0075cd002ee9b063
woosung1223 <dntjd991223@naver.com>
2024-04-09 01:53:47 +0900

    feat: 네비게이션 바 스타일 변경, 채팅 Axios 요청 추가

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,14 +1,19 @@
 <template>
   <v-container>
     <!-- 좌측 네비게이션 바 부분 -->
-    <v-navigation-drawer class="bg-blue-grey-lighten-3 rounded-e-lg border-0">
+    <v-navigation-drawer class="bg-blue-grey-lighten-4 rounded-e-lg border-0">
       <!-- <v-list-item link class="bg-blue-grey-lighten-3">new Chat</v-list-item> -->
       <v-list-item v-for="(roomName, index) in roomNames" :key="index">
-        <v-list-item link class="border-b-sm" style="color:white; text-shadow: 1px 1px 1px #263238;">{{ roomName.value }}</v-list-item>
+        <v-list-item
+          link
+          class="border-b-sm"
+          style="color: white; text-shadow: 0.5px 0.5px 0.5px #263238"
+          >{{ roomName.value }}</v-list-item
+        >
       </v-list-item>
     </v-navigation-drawer>
 
-    <v-card class="chat-box" flat>
+    <v-card flat>
       <v-list>
         <v-list-item v-for="(message, index) in messages" :key="index">
           <!-- 사용자 아이콘 + 텍스트 부분 -->
@@ -24,10 +29,10 @@
           <!-- AI 아이콘 + 텍스트 부분 -->
           <v-card
             v-if="!message.isUser"
-            append-icon="mdi-robot-happy-outline"
+            prepend-icon="mdi-robot-happy-outline"
             elevation="4"
           >
-            <v-card-text align="right">{{ message.text }}</v-card-text>
+            <v-card-text class="typewriter">{{ message.text }}</v-card-text>
           </v-card>
         </v-list-item>
       </v-list>
@@ -52,6 +57,8 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "Chat",
   data() {
@@ -64,23 +71,37 @@ export default {
   },
   methods: {
     sendMessage() {
+      this.messages.push({ isUser: true, text: this.newMessage });
+      var interMessages = this.messages;
       if (this.newMessage.trim() !== "") {
-        this.messages.push({ isUser: true, text: this.newMessage });
-        this.messages.push({ isUser: false, text: this.newMessage });
+        console.log(process.env.VUE_APP_API_SERVER)
+        axios
+          .post(
+            process.env.VUE_APP_API_SERVER,
+            {
+              prompt: this.newMessage,
+            },
+            {
+              "Content-Type": "application/json",
+            }
+          )
+          .then(function (response) {
+            console.log(response.data.data)
+            var data = response.data.data;
+            var modelIndex = data.indexOf("model");
+            data = data.slice(modelIndex + "model".length);
+
+            interMessages.push({ isUser: false, text: data });
+          })
+          .catch(function (error) {
+            // 예외처리
+            console.log(error);
+          });
+
         this.newMessage = ""; // 메시지 전송 후 입력 필드를 비웁니다.
-        this.roomNames.push({ value: "스프링 부트 면접" });
+        // this.roomNames.push({ value: "스프링 부트 면접" });
       }
     },
   },
 };
 </script>
-
-<style>
-.flex {
-  display: flex;
-}
-
-.align-center {
-  align-items: center;
-}
-</style>
