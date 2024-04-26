d9cfa2b03c8b64ee7f256676b10d460b4c2d4a48
woosung1223 <dntjd991223@naver.com>
2024-04-26 17:05:56 +0900

    refactor: 실제 대화를 저장하는 Conversation 컴포넌트 분리

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -2,62 +2,23 @@
   <v-container>
     <v-row>
       <v-col>
-        <v-card flat>
-          <v-list>
-            <template v-if="messages.length !== 0">
-              <v-list-item v-for="(message, index) in messages" :key="index">
-                <!-- 사용자 아이콘 + 텍스트 부분 -->
-
-                <v-card
-                  v-if="message.isUser"
-                  prepend-icon="mdi-account"
-                  elevation="4"
-                >
-                  <v-card-text max-width="100">{{ message.text }}</v-card-text>
-                </v-card>
-
-                <!-- AI 아이콘 + 텍스트 부분 -->
-                <v-card
-                  v-if="!message.isUser"
-                  prepend-icon="mdi-robot-happy-outline"
-                  elevation="4"
-                >
-                  <v-card-text class="typewriter">{{
-                    message.text
-                  }}</v-card-text>
-                </v-card>
-              </v-list-item>
+        <Conversation :messages="this.messages"></Conversation>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <!-- 텍스트 필드 부분 -->
+          <v-text-field
+            v-model="writingMessage"
+            label="write an answer."
+            flat
+            rounded
+            variant="solo-filled"
+            @keyup.enter="sendMessage"
+          >
+            <template #append>
+              <v-btn color="secondary" @click="sendMessage"> Send </v-btn>
             </template>
-
-            <!-- 아무 메세지도 없을 시, Welcome 출력 -->
-            <template v-else>
-              <v-list-item style="height: 100%">
-                <v-card flat style="height: 100%">
-                  <v-card-text
-                    class="fill-height d-flex align-center justify-center"
-                    >Start a conversation...</v-card-text
-                  >
-                </v-card>
-              </v-list-item>
-            </template>
-          </v-list>
-          <v-divider></v-divider>
-          <v-card-actions>
-            <!-- 텍스트 필드 부분 -->
-            <v-text-field
-              v-model="writingMessage"
-              label="write an answer."
-              flat
-              rounded
-              variant="solo-filled"
-              @keyup.enter="sendMessage"
-            >
-              <template #append>
-                <v-btn color="secondary" @click="sendMessage"> Send </v-btn>
-              </template>
-            </v-text-field>
-          </v-card-actions>
-        </v-card>
+          </v-text-field>
+        </v-card-actions>
       </v-col>
     </v-row>
   </v-container>
@@ -66,26 +27,24 @@
 <script>
 import axios from "axios";
 
+import Conversation from "./Conversation.vue"
+
 export default {
   name: "Chat",
 
   data() {
     return {
       messages: [],
-      roomNames: [],
       writingMessage: "",
-      isNavigationVisible: true,
     };
   },
 
   methods: {
     sendMessage() {
-      // 컨텍스트 공유를 위한 변수
       const interMessages = this.messages;
       if (this.writingMessage.trim() !== "") {
-        // 우선 유저가 보낸 메세지 저장
         this.messages.push({ isUser: true, text: this.writingMessage });
-        // 프롬프트 요청
+
         axios
           .post(
             process.env.VUE_APP_API_SERVER,
@@ -110,5 +69,9 @@ export default {
       }
     },
   },
+
+  components: {
+    Conversation,
+  }
 };
 </script>
---
+++ src/components/Conversation.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-card flat>
+    <v-list>
+      <template v-if="messages.length !== 0">
+        <v-list-item v-for="(message, index) in messages" :key="index">
+          <!-- 사용자 아이콘 + 텍스트 부분 -->
+          <v-card
+            v-if="message.isUser"
+            prepend-icon="mdi-account"
+            elevation="4"
+          >
+            <v-card-text max-width="100">{{ message.text }}</v-card-text>
+          </v-card>
+
+          <!-- AI 아이콘 + 텍스트 부분 -->
+          <v-card
+            v-if="!message.isUser"
+            prepend-icon="mdi-robot-happy-outline"
+            elevation="4"
+          >
+            <v-card-text class="typewriter">{{ message.text }}</v-card-text>
+          </v-card>
+        </v-list-item>
+      </template>
+
+      <!-- 아무 메세지도 없을 시, Welcome 출력 -->
+      <template v-else>
+        <v-list-item style="height: 100%">
+          <v-card flat style="height: 100%">
+            <v-card-text class="fill-height d-flex align-center justify-center"
+              >Start a conversation...</v-card-text
+            >
+          </v-card>
+        </v-list-item>
+      </template>
+    </v-list>
+  </v-card>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Conversation",
+
+  props: {
+    messages: {
+        type: Object,
+    }
+  }
+});
+</script>
