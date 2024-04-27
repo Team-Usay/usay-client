83d99bc15e90ab60266192233263c5b8d6af40f2
woosung1223 <dntjd991223@naver.com>
2024-04-27 18:32:52 +0900

    chore: 채팅 스크롤 기능 추가

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,33 +1,34 @@
 <template>
   <v-container>
-    <v-row>
-      <v-col>
-        <Conversation :messages="this.messages"></Conversation>
-        <v-divider></v-divider>
-        <v-card-actions>
-          <!-- 텍스트 필드 부분 -->
-          <v-text-field
-            v-model="writingMessage"
-            label="write an answer."
-            flat
-            rounded
-            variant="solo-filled"
-            @keyup.enter="sendMessage"
-          >
-            <template #append>
-              <v-btn color="secondary" @click="sendMessage"> Send </v-btn>
-            </template>
-          </v-text-field>
-        </v-card-actions>
-      </v-col>
-    </v-row>
+    <div style="height: 90vh;"> <!-- Conversation 부분의 부모 요소 -->
+      <Conversation :messages="messages" style="height: 100%;"></Conversation>
+    </div>
+    <v-divider></v-divider>
+
+    <div style="height: 10vh;"> <!-- Conversation 부분의 부모 요소 -->
+    <v-card-actions>
+      <!-- 텍스트 필드 부분 -->
+      <v-text-field
+        v-model="writingMessage"
+        label="write an answer."
+        flat
+        rounded
+        variant="solo-filled"
+        @keyup.enter="sendMessage"
+      >
+        <template #append>
+          <v-btn color="orange" @click="sendMessage"> Send </v-btn>
+        </template>
+      </v-text-field>
+    </v-card-actions>
+    </div>
   </v-container>
 </template>
 
 <script>
-import axios from "axios";
+// import axios from "axios";
 
-import Conversation from "./Conversation.vue"
+import Conversation from "./Conversation.vue";
 
 export default {
   name: "Chat",
@@ -41,30 +42,29 @@ export default {
 
   methods: {
     sendMessage() {
-      const interMessages = this.messages;
       if (this.writingMessage.trim() !== "") {
         this.messages.push({ isUser: true, text: this.writingMessage });
 
-        axios
-          .post(
-            process.env.VUE_APP_API_SERVER,
-            {
-              prompt: this.writingMessage,
-            },
-            {
-              "Content-Type": "application/json",
-            }
-          )
-          .then(function (response) {
-            const data = response.data.data;
-            const modelIndex = data.indexOf("model");
-            const modelAnswer = data.slice(modelIndex + "model".length);
+        // axios
+        //   .post(
+        //     process.env.VUE_APP_API_SERVER,
+        //     {
+        //       prompt: this.writingMessage,
+        //     },
+        //     {
+        //       "Content-Type": "application/json",
+        //     }
+        //   )
+        //   .then(function (response) {
+        //     const data = response.data.data;
+        //     const modelIndex = data.indexOf("model");
+        //     const modelAnswer = data.slice(modelIndex + "model".length);
 
-            interMessages.push({ isUser: false, text: modelAnswer });
-          })
-          .catch(function (error) {
-            console.error(error);
-          });
+        //     this.messages.push({ isUser: false, text: modelAnswer });
+        //   })
+        //   .catch(function (error) {
+        //     console.error(error);
+        //   });
         this.writingMessage = "";
       }
     },
@@ -72,6 +72,14 @@ export default {
 
   components: {
     Conversation,
-  }
+  },
 };
 </script>
+
+<style scoped>
+.card-actions {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+}
+</style>
--- src/components/Conversation.vue
+++ src/components/Conversation.vue
@@ -1,40 +1,36 @@
 <template>
-  <v-card flat>
-    <v-list>
-      <template v-if="messages.length !== 0">
-        <v-list-item v-for="(message, index) in messages" :key="index">
-          <!-- 사용자 아이콘 + 텍스트 부분 -->
-          <v-card
-            v-if="message.isUser"
-            prepend-icon="mdi-account"
-            elevation="4"
-          >
-            <v-card-text max-width="100">{{ message.text }}</v-card-text>
-          </v-card>
+  <v-list style="overflow-y: auto; max-height: 100%">
+    <template v-if="messages.length !== 0">
+      <v-list-item v-for="(message, index) in messages" :key="index">
+        <!-- 사용자 아이콘 + 텍스트 부분 -->
+        <v-card v-if="message.isUser" prepend-icon="mdi-account" elevation="4">
+          <v-card-text max-width="100">{{ message.text }}</v-card-text>
+        </v-card>
 
-          <!-- AI 아이콘 + 텍스트 부분 -->
-          <v-card
-            v-if="!message.isUser"
-            prepend-icon="mdi-robot-happy-outline"
-            elevation="4"
-          >
-            <v-card-text class="typewriter">{{ message.text }}</v-card-text>
-          </v-card>
-        </v-list-item>
-      </template>
+        <!-- AI 아이콘 + 텍스트 부분 -->
+        <v-card
+          v-if="!message.isUser"
+          prepend-icon="mdi-robot-happy-outline"
+          elevation="4"
+        >
+          <v-card-text class="typewriter">{{ message.text }}</v-card-text>
+        </v-card>
+      </v-list-item>
+    </template>
 
-      <!-- 아무 메세지도 없을 시, Welcome 출력 -->
-      <template v-else>
-        <v-list-item style="height: 100%">
-          <v-card flat style="height: 100%">
-            <v-card-text class="fill-height d-flex align-center justify-center"
-              >Start a conversation...</v-card-text
-            >
-          </v-card>
-        </v-list-item>
-      </template>
-    </v-list>
-  </v-card>
+    <!-- 아무 메세지도 없을 시, Welcome 출력 -->
+    <template v-else>
+      <v-list-item style="height: 100%">
+        <v-card flat style="height: 100%">
+          <v-card-text class="fill-height d-flex align-center justify-center">
+            <h2 class="text-h10 font-weight-black text-orange">
+              Start a conversation
+            </h2></v-card-text
+          >
+        </v-card>
+      </v-list-item>
+    </template>
+  </v-list>
 </template>
 
 <script>
@@ -42,11 +38,6 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "Conversation",
-
-  props: {
-    messages: {
-        type: Object,
-    }
-  }
+  props: ["messages"],
 });
 </script>
